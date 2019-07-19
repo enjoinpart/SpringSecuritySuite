bb298ed4e93ae6e00a8fb7269e96a7ea75074772
admin <307125515@qq.com>
2019-07-19 11:53:33 +0800

    -------

--- src/main/java/com/github/edu/controller/RedisController.java
+++ src/main/java/com/github/edu/controller/RedisController.java
@@ -2,6 +2,7 @@ package com.github.edu.controller;
 
 import com.github.edu.base.controller.APIResponse;
 import com.github.edu.base.controller.BaseController;
+import com.github.edu.base.utils.RedisUtils;
 import com.github.edu.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,20 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/redis/")
 public class RedisController extends BaseController {
 
-    @Autowired
-    private RedisService redisService;
+    private RedisUtils redisUtils;
 
+    @Autowired
+    public RedisController(RedisUtils redisUtils) {
+        this.redisUtils = redisUtils;
+    }
 
     @GetMapping("test")
     public APIResponse redisTest() {
-        String s = (String) redisService.get("lisi");
+        String s = (String) redisUtils.get("lisi");
         System.out.println("s==" + s);
         return success(s);
     }
 
     @GetMapping("set")
     public APIResponse setRedisStr() {
-        redisService.set("lisi", "lisi123456");
+        redisUtils.set("lisi", "lisi123456");
         return success(true);
     }
 
